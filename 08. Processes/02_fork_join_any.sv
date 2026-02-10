2430d1fb4bf53ae6e5310139750f661e99c3d1b0
Sathyan S S <sssathyan2004@gmail.com>
2026-02-10 20:17:58 +0530

    Create 02_fork_join_any.sv

---
+++ 08. Processes/02_fork_join_any.sv
@@ -0,0 +1,37 @@
+module fork_join_any();
+  
+  initial begin
+    
+    fork: fork_join_any
+      
+      begin: Part_A
+        
+        $display("This is Part A");
+        #10;
+        $display("Part A process completed");
+        
+      end: Part_A
+      
+      begin: Part_B
+        
+        $display("This is Part B");
+        #11;
+        $display("Part B process completed");
+        
+      end: Part_B
+      
+      begin: Part_C
+        
+        $display("This is Part C");
+        #12;
+        $display("Part C process completed");
+        
+      end: Part_C
+      
+    join_any: fork_join_any
+    
+    $display("\nFork join any process over\n");
+    
+  end
+  
+endmodule

363c4e2006b248d2515bac94c0bc3b36ff0f0ba0
Sathyan S S <sssathyan2004@gmail.com>
2026-02-10 20:30:50 +0530

    Create 05_disable_fork.sv

---
+++ 08. Processes/05_disable_fork.sv
@@ -0,0 +1,40 @@
+module fork_join_example();
+  
+  initial begin
+    
+    fork: fork_join
+      
+      begin: Part_A
+        
+        $display("This is Part A");
+        #10;
+        $display("Part A process completed");
+        
+      end: Part_A
+      
+      begin: Part_B
+        
+        $display("This is Part B");
+        #11;
+        $display("Part B process completed");
+        
+      end: Part_B
+      
+      begin: Part_C
+        
+        $display("This is Part C");
+        #12;
+        $display("Part C process completed");
+        
+      end: Part_C
+      
+    join_any: fork_join
+    
+    disable fork_join.Part_B;
+    disable fork_join.Part_C;
+      
+    $display("\nFork join any process over\n");
+    
+  end
+  
+endmodule

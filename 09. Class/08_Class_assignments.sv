2571f9d9eb79e9a8eb397cb4df2ea9bbad225bbe
Sathyan S S <sssathyan2004@gmail.com>
2026-02-12 14:04:46 +0530

    Create 08_Class_assignments.sv

---
+++ 09. Class/08_Class_assignments.sv
@@ -0,0 +1,20 @@
+class testing;
+  bit [3:0] data;
+  function new();
+    data = 4'b1111;
+  endfunction
+endclass
+
+module testing_module();
+  initial begin
+    testing t1, t2;
+    t1 = new();
+    t2 = t1;
+    $display("T1 data: %b",t1.data);
+    $display("T2 data: %b",t2.data);
+    t1.data = 4'b0000;
+    t2.data = 4'b1010;
+    $display("T1 data: %b",t1.data);
+    $display("T2 data: %b",t2.data);
+  end
+endmodule

44dcc159e7ac016f323511dbfb8719873707e850
Sathyan S S <sssathyan2004@gmail.com>
2026-02-12 21:09:09 +0530

    Create 17_Encapsulation_local.sv

---
+++ 09. Class/17_Encapsulation_local.sv
@@ -0,0 +1,17 @@
+class parent;
+  local int data=10;
+endclass
+
+class child extends parent;
+  function void display();
+    $display("Value of data inside child: %0d",data);
+  endfunction
+endclass
+
+module encapsulation();
+  initial begin
+  	child c;
+    c=new();
+  	c.display();
+  end
+endmodule
